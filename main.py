9351fecb1893741bd8c4202e59edd267fae3a061
Alec German <kimarimt@protonmail.com>
2024-11-04 09:20:05 -0500

    add player to updatable and drawable sprite groups

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from player import Player
 
 def main():
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(x, y)
     clock = pygame.time.Clock()
     dt = 0
@@ -18,8 +25,12 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
